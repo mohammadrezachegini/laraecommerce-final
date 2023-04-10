c654f02c800abc461a393ee665d9589a0278582e
Reza <mohammadreza.chegini01@gmail.com>
2023-04-10 09:48:14 -0700

    first commit

---
+++ resources/views/layouts/inc/frontend/footer.blade.php
@@ -0,0 +1,20 @@
+<div>
+    <div class="footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+
+                    <h4 class="footer-heading">{{ $appSetting->website_name ?? 'website name' }}</h4>
+                    <div class="footer-underline"></div>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
